70542d3c8bbdc801cc8485def7d7f94befed97e5
Sarah <dousse.sarah@gmail.com>
2022-03-07 23:39:19 +0000

    add and setup gem stimulus-rails

---
+++ app/javascript/controllers/address_controller.js
@@ -0,0 +1,8 @@
+// ./bin/rails stimulus:manifest:update 
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  connect() {
+    console.log('Address controller is connected');
+  }
+}
