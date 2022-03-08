337e92dff308d706ad6c981b737694163cb942fe
Sarah <dousse.sarah@gmail.com>
2022-03-08 22:54:25 +0000

    add users/ omniauth_controller, add columns to Users table for name and uid, add methods to user model for google_oauth2

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :listings
   end
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root 'static_pages#home'
 end
